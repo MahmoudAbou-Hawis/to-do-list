5ae678f036316e07656efb16448b7c536860c059
Mahmoud Abou-Hawis <70099897+MahmoudAbou-Hawis@users.noreply.github.com>
2025-01-09 16:28:19 +0200

    Delete build directory

--- build/CMakeFiles/to-do-list.dir/to-do-list_autogen/QG6UEUUZRY/qrc_qml.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/to-do-list.dir/to-do-list_autogen/QG6UEUUZRY/qrc_qml.cpp.o: \
- /home/mahmoud/Documents/to-do-list/build/to-do-list_autogen/QG6UEUUZRY/qrc_qml.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/to-do-list_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/to-do-list_autogen"
-  "to-do-list_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/to-do-list_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
