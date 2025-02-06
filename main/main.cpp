b411551ba3e3350bf79e95ac6a4339c6dfd4aae4
MahmoudAbou-Hawis <mahmoud.abohawis10@gmail.com>
2025-02-06 21:28:22 +0200

    Added: edit main for load login

--- main/main.cpp
+++ main/main.cpp
@@ -2,22 +2,32 @@
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include <QQuickView>
-#include "../GUI/src/wrappers/taskwrapper.hpp"
-#include "../GUI/src/gui-initalizer/guiinitializer.hpp"
+#include "../gui/src/wrappers/taskwrapper.hpp"
+#include "../gui/src/gui-initalizer/guiinitializer.hpp"
 #include "../tasks/src/core-initializer/coreinitializer.hpp"
-
+#include "plugin-support/src/pluginloader.hpp"
+#include <spdlog/sinks/rotating_file_sink.h>
+#include <spdlog/spdlog.h>
+#include "../tasks/src/adapters/pluginAdapter.hpp"
+#include "../tasks/src/primitives/task.hpp"
+#include <string>
+#include <chrono>
+#include <thread>
+#include <iostream>
+#include "../gui/src/gui-initalizer/loginintalizer.hpp"
+#include <QTimer>
 int main(int argc, char *argv[])
 {
 
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+    spdlog::rotating_logger_mt("root_logger", "to-do.log", 1048576 * 5,
+                             3);
 
     
-    GuiInitializer g(&engine,std::make_shared<CoreInitializer>());
-
-
-    const QUrl url(QStringLiteral("qrc:/GUI/src/qml/main.qml"));
+    LoginInitializer l(&engine);
+    const QUrl url(QStringLiteral("qrc:/gui/src/qml/login.qml"));
 
     engine.load(url);
 
